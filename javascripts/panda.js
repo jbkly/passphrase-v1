68e7482e22da00abf1aa7d450eb4cea3558b67bd
Jordan Buckley <jordan@bkly.org>
2016-01-25 18:25:34 -0800

    add logic for handling different capitalizations

--- javascripts/panda.js
+++ javascripts/panda.js
@@ -1,18 +1,44 @@
 $(function() {
 
   // user options - words to use, separator, numbers, special chars
-  var generatedPhrase,
-      wordsToUse = 4,
-      wordArray = localStorage.wordArray ? JSON.parse(localStorage.wordArray) : window.wordArray;
+  var options = {
+    generatedPhrase: '',
+    wordCount: 4,
+    capitals: 'startCase',
+    separator: ' ',
+    wordList: localStorage.wordArray ? JSON.parse(localStorage.wordArray) : window.wordArray,
+    words: []
+  };
 
   // generate function on user click
   $('#generate').click(function() {
 
     // build phrase using lodash sampleSize - is this random enough?
-    generatedPhrase = _.sampleSize(wordArray, wordsToUse).join(' ');
+    options.words = _.sampleSize(options.wordList, options.wordCount);
+
+    switch (options.capitals) {
+      case 'startCase':
+        options.words.forEach(function(word, i, words) {
+          words[i] = _.upperFirst(word);
+        });
+        break;
+      case 'upperCase':
+        options.words.forEach(function(word, i, words) {
+          words[i] = word.toUpperCase();
+        });
+        break;
+      case 'lowerCase':
+        options.words.forEach(function(word, i, words) {
+          words[i] = word.toLowerCase();
+        });
+        break;
+      // kebab case? camel case? snake case?
+    }
+
+    options.generatedPhrase = options.words.join(options.separator);
 
     // display generated phrase and automatically copy to clipboard - add fallback to zeroclipboard for older browsers? make sure it works cross-browser or at least doesn't mislead user
-    $('#passphrase .generated').val(generatedPhrase).select();
+    $('#passphrase .generated').val(options.generatedPhrase).select();
 
     try {
       // Now that we've selected the text, execute the copy command
@@ -28,7 +54,5 @@ $(function() {
     }
 
   });
-
-
 
 });
