f0c02891065487234ac81dedc0e4d41a9e271744
Jordan Buckley <jordan@bkly.org>
2016-02-14 16:12:22 -0800

    copy to clipboard working with fallback message for safari

--- components/app.jsx
+++ components/app.jsx
@@ -18,57 +18,71 @@ const PassphraseGenerator = React.createClass({
   },
   generatePhrase: function() {
     // build phrase using lodash sampleSize - is this random enough?
-    this.state.words = _.sampleSize(this.state.wordList, this.state.wordCount);
+    let words = _.sampleSize(this.state.wordList, this.state.wordCount);
 
     switch (this.state.capitals) {
       case 'startCase':
-        this.state.words.forEach(function(word, i, words) {
-          words[i] = _.upperFirst(word);
-        });
+        words.forEach((word, i, words) => words[i] = _.upperFirst(word));
         break;
       case 'upperCase':
-        this.state.words.forEach(function(word, i, words) {
-          words[i] = word.toUpperCase();
-        });
+        words.forEach((word, i, words) => words[i] = word.toUpperCase());
         break;
       case 'lowerCase':
-        this.state.words.forEach(function(word, i, words) {
-          words[i] = word.toLowerCase();
-        });
+        words.forEach((word, i, words) => words[i] = word.toLowerCase());
         break;
       // kebab case? camel case? snake case?
     }
 
-    this.state.generatedPhrase = this.state.words.join(this.state.separator);
-
-    // display generated phrase and automatically copy to clipboard - add fallback to zeroclipboard for older browsers? make sure it works cross-browser or at least doesn't mislead user
-    $('#passphrase .generated').val(this.state.generatedPhrase).select();
+    let generatedPhrase = words.join(this.state.separator);
+    this.setState({words, generatedPhrase}, this.copyToClipboard);
+  },
+  copyToClipboard: function() {
+    this.logCurrentPhrase();
+    this.phraseInput.select();
 
     try {
       // Now that we've selected the text, execute the copy command
       var successful = document.execCommand('copy');
       if (successful) {
-        $('#passphrase .copied-success').show().delay(2000).fadeOut(1000);
+        $('#passphrase .copied-success').show().delay(3000).fadeOut(1000);
 
         // Remove the selections
         window.getSelection().removeAllRanges();
+      } else {
+        this.showAltMessage();
       }
     } catch(err) {
-      console.log('Oops, unable to copy');
+      this.showAltMessage();
     }
+  },
+  handlePhraseChange: function(event) {
+    this.setState({generatedPhrase: event.target.value}, this.copyToClipboard);
+  },
+  logCurrentPhrase: function() {
+    console.log(`${this.state.generatedPhrase}`);
+  },
+  showAltMessage: function() {
+    $('#passphrase .copy-failed').show().delay(4000).fadeOut(2000);
   },
   render: function() {
     return (
       <section>
         <div id="title">
-          <h1>Passphrase Pam</h1>
+          <h1>Passphrase {Date.now()}</h1>
           <p>Instantly generate secure, memorable passphrases</p>
         </div>
 
         <div id="passphrase">
-          <input type="text" className="generated" />
+          <input
+            type="text"
+            className="generated"
+            ref={(input) => this.phraseInput = input}
+            value={this.state.generatedPhrase}
+            onChange={this.handlePhraseChange}
+          />
           <button id="generate" className="go" onClick={this.generatePhrase} >Generate</button>
           <p className="copied-success">Your phrase has been copied to your clipboard</p>
+          <p className="copy-failed">Press &#8984;+C (Mac) or Ctrl+C (Windows) to copy your passphrase</p>
         </div>
       </section>
     );
