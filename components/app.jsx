8e8e67b76661967a4877d3f0d43d7668a565b882
Jordan Buckley <jordan@bkly.org>
2019-06-03 05:55:02 +0000

    rebase on gh-pages, resolve conflict in app.jsx

--- components/app.jsx
+++ components/app.jsx
@@ -1,46 +1,74 @@
 import React from 'react';
-import { render as renderDOM} from 'react-dom';
-import _ from 'lodash';
+import { render as renderDOM } from 'react-dom';
+import { CASING_OPTIONS, DEFAULT_OPTIONS } from './options';
+import _ from 'lodash'; // todo: import only modules needed from lodash
+
+// todo: redo with updated dependencies (react, babel, yarn)
+// todo: over-engineer the state management
+// todo: internationalization?
 
 const PassphraseGenerator = React.createClass({
+  // todo: get/save options in localstorage
+  // todo: proptypes/typing
   getInitialState: function() {
     return {
       generatedPhrase: '',
-      wordCount: 4,
-      capitals: 'startCase',
-      separator: ' ',
       wordList: localStorage.wordArray ? JSON.parse(localStorage.wordArray) : window.wordArray,
-      words: []
+      words: [],
+      options: DEFAULT_OPTIONS,
     }
   },
-  componentDidMount: function() {
-
-  },
   generatePhrase: function() {
+    console.log('this.state.options: ', this.state.options);
     // build phrase using lodash sampleSize - is this random enough?
-    let words = _.sampleSize(this.state.wordList, this.state.wordCount);
+    let words = [];
+    // defaults for now - allow more customization
+    let characterLimit = this.state.options.characterLimit ? 20 : Number.MAX_SAFE_INTEGER;
+    let separator = this.state.options.useSpaces ? ' ' : '';
 
-    switch (this.state.capitals) {
-      case 'startCase':
-        words.forEach((word, i, words) => words[i] = _.upperFirst(word));
-        break;
-      case 'upperCase':
+    // generate a set of words until one is found within 
+    do {
+      let wordCount = this.state.options.wordCount;
+      let number = null;
+      if (this.state.options.includeNumbers) {
+        number = getRandomInt(0, 999);
+        wordCount--;
+      }
+
+      words = _.sampleSize(this.state.wordList, wordCount);
+
+      if (number) {
+        let randomIndex = getRandomInt(0, wordCount + 1);
+        words.splice(randomIndex, 0, number);
+      }
+    } while (this.checkPhraseLength(words) > characterLimit);
+
+    // move logic with options?
+    switch (this.state.options.casing) {
+      case CASING_OPTIONS.upperCase:
         words.forEach((word, i, words) => words[i] = word.toUpperCase());
         break;
-      case 'lowerCase':
+      case CASING_OPTIONS.lowerCase:
         words.forEach((word, i, words) => words[i] = word.toLowerCase());
+        break;
+      case CASING_OPTIONS.startCase:
+      default:
+        words.forEach((word, i, words) => words[i] = _.upperFirst(word));
         break;
       // TODO: kebab case? camel case? snake case?
     }
 
-    let generatedPhrase = words.join(this.state.separator);
+    let generatedPhrase = words.join(separator);
     this.setState({words, generatedPhrase}, this.copyToClipboard);
+  },
+  checkPhraseLength: function(words) {
+    return words.join(this.state.options.separator).length;
   },
   copyToClipboard: function() {
     // store initial cursor position
     let el = this.phraseInput,
-        selectionStart = el.selectionStart,
-        selectionEnd = el.selectionEnd;
+      selectionStart = el.selectionStart,
+      selectionEnd = el.selectionEnd;
 
     if (!el.value) return;
 
@@ -66,7 +94,17 @@ const PassphraseGenerator = React.createClass({
 
   },
   handlePhraseChange: function(event) {
-    this.setState({generatedPhrase: event.target.value}, this.copyToClipboard);
+    this.setState({ generatedPhrase: event.target.value }, this.copyToClipboard);
+  },
+  onOptionsChange: function(optionChanged) {
+    console.log('option changed: ', optionChanged);
+
+    if (optionChanged.hasOwnProperty('useSpaces')) {
+      optionChanged.separator = optionChanged.useSpaces ? ' ' : '';
+    }
+
+    const updatedOptions = Object.assign(this.state.options, optionChanged);
+    this.setState({ options: updatedOptions });
   },
   showAltMessage: function() {
     $('#passphrase .copy-failed').show().delay(4000).fadeOut(2000);
@@ -76,18 +114,19 @@ const PassphraseGenerator = React.createClass({
 
     return (
       <section>
-        <div id="title">
+        <header id="title">
           <h1>Passphrase</h1>
           <p className="lead">Instantly generate secure, memorable passphrases</p>
-        </div>
+        </header>
 
-        <div id="passphrase">
+        <main id="passphrase">
           <input
             type="text"
             className="generated"
             ref={(input) => this.phraseInput = input}
             value={this.state.generatedPhrase}
             onChange={this.handlePhraseChange}
+            placeholder="Your next passphrase..."
           />
           <div className="button-group">
             <DisplayCharCount charCount={phrase.length} />
@@ -103,12 +142,16 @@ const PassphraseGenerator = React.createClass({
             <p className="copied-success">Your phrase has been copied to your clipboard</p>
             <p className="copy-failed">Press &#8984;+C (Mac) or Ctrl+C (Windows) to copy your passphrase</p>
           </div>
-          <OptionsPanel />
-        </div>
+          <OptionsPanel
+            {...this.state.options}
+            onChange={this.onOptionsChange}
+          />
+        </main>
         <footer>
-          <span className="credits">
-            <a href="https://github.com/jbkly/passphrase">View the Project on GitHub</a>
-          </span>
+          <article className="credits">
+            <aside><a href="https://xkcd.com/936/">Why passphrases are better than passwords</a></aside>
+            <aside><a href="https://github.com/jbkly/passphrase">View the Project on GitHub</a></aside>
+          </article>
         </footer>
       </section>
     );
@@ -131,7 +174,7 @@ const DisplayCharCount = React.createClass({
 const CopyButton = React.createClass({
   render: function() {
     if (this.props.phrase) {
-     return (
+      return (
         <button
           id="copy-to-clipboard"
           className="secondary"
@@ -143,24 +186,40 @@ const CopyButton = React.createClass({
   }
 });
 
+// todo: factor this component out
 const OptionsPanel = React.createClass({
+  handleChange: function(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.props.onChange && this.props.onChange({ [name]: value });
+  },
   render: function() {
-    return null;
-    // return (
-    //   <form className="options-panel">
-    //     <h3>Options</h3>
-    //     <label className="option">
-    //       <input type="checkbox" value="limitChars" />
-    //       &nbsp;Limit characters
-    //     </label>
-    //     <label className="option">
-    //       <input type="checkbox" value="includeNums" />
-    //       &nbsp;Include numbers
-    //     </label>
-    //   </form>
-    // );
+    return (
+      <form className="options-panel">
+        <h3>Options</h3>
+        <label className="option">
+          <input type="checkbox" name="includeNumbers" checked={this.props.includeNumbers} onChange={this.handleChange} />
+          &nbsp;Include numbers
+        </label>
+          <label className="option">
+          <input type="checkbox" name="useSpaces" checked={this.props.useSpaces} onChange={this.handleChange} />
+          &nbsp;Use spaces
+        </label>
+        <label className="option">
+          <input type="checkbox" name="characterLimit" checked={this.props.characterLimit} onChange={this.handleChange} />
+          &nbsp;Limit characters
+        </label>
+      </form>
+    );
   }
 });
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 renderDOM(
   <PassphraseGenerator />,
